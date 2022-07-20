4bebdabc60b0363bb5dbc10ae778bb0fc83b1c65
shuaoz <shuaoz@umich.edu>
2022-07-19 20:41:47 -0400

    tests: add more unit tests

--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -1,11 +1,40 @@
 package auth
 
 import (
+	"context"
+	"encoding/hex"
 	"encoding/json"
+	"github.com/filecoin-project/venus-auth/config"
+	"github.com/filecoin-project/venus-auth/storage"
+	"github.com/gbrlsnchs/jwt/v3"
+	"github.com/stretchr/testify/assert"
 	"testing"
-
-	"github.com/magiconair/properties/assert"
 )
+
+func init() {
+	secret, err := config.RandSecret()
+	if err != nil {
+		panic(err)
+	}
+	sec, err := hex.DecodeString(hex.EncodeToString(secret))
+	if err != nil {
+		panic(err)
+	}
+	jwtOAuthInstance = &jwtOAuth{
+		secret: jwt.NewHS256(sec),
+		store:  storage.MockStore{},
+		mp:     newMapper(),
+	}
+}
+
+func TestGetToken(t *testing.T) {
+	tokenInfo, err := jwtOAuthInstance.GetToken(context.Background(), storage.MockToken)
+	assert.Nil(t, err)
+	assert.Equal(t, string(storage.MockKeyPair.Token), tokenInfo.Token)
+	assert.Equal(t, storage.MockKeyPair.Perm, tokenInfo.Perm)
+}
+
+// TODO: More tests
 
 func TestTokenDecode(t *testing.T) {
 	payload := []byte("eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ")
